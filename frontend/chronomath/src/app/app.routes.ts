d05d7d456ae424b0855d15cd5c7ae84dd55b21fc
FahadBhatti424 <fahad.bhatti424@gmail.com>
2025-03-10 19:51:29 -0400

    Added components

--- frontend/chronomath/src/app/app.routes.ts
+++ frontend/chronomath/src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TimelineComponent } from './timeline/timeline.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignupComponent } from './signup/signup.component';
+import { LessonComponent } from './lesson/lesson.component';
+
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    { path: 'timeline', component: TimelineComponent }
+    { path: 'timeline', component: TimelineComponent },
+    { path: 'login', component:  LoginComponent},
+    { path: 'profile', component: ProfileComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: 'lesson', component:  LessonComponent}  
 ];
