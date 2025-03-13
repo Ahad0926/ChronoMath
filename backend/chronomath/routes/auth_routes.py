4be18a3131e8878166c73539eefaec215409ed7c
MdTanjeemHaider <mdtanjeem.haider@ontariotechu.net>
2025-03-12 23:41:38 -0400

    Add session check endpoint + update user creation in database

--- backend/chronomath/routes/auth_routes.py
+++ backend/chronomath/routes/auth_routes.py
@@ -4,6 +4,21 @@ from google.cloud.firestore_v1 import FieldFilter
 
 auth_bp = Blueprint("auth", __name__)
 
+# Copies a document and all its subcollections
+def copy_document(source, destination):
+    docs = source.get()
+
+    for doc in docs:
+        # Copy document
+        doc_data = doc.to_dict()
+        destination_doc_ref = destination.document(doc.id)
+        destination_doc_ref.set(doc_data)
+
+        # Copy subcollections
+        subcollections = source.document(doc.id).collections()
+        for subcollection in subcollections:
+            copy_document(subcollection, destination_doc_ref.collection(subcollection.id))
+
 @auth_bp.route('/register', methods=["POST"])
 def register():
     try:
@@ -23,13 +38,21 @@ def register():
         user = auth.create_user_with_email_and_password(email, password)
 
         # Add user to database
-        users_ref.document(user["localId"]).set({
+        user_data = users_ref.document(user["localId"])
+        user_data.set({
             "email": email,
-            "name": name
+            "name": name,
+            "points": 0 
         })
+        
+        # Add Collections from template to user
+        template = db.collection("Era")
+        user_lesson_collection = user_data.collection("Era")
+        copy_document(template, user_lesson_collection)
 
         # Create session
         session['token'] = user['idToken']
+        session['uuid'] = user["localId"]
         session['email'] = email
 
         return jsonify({"Success": "User created"}), 200
@@ -78,3 +101,10 @@ def forgot_password():
         return jsonify({"Success": "Password reset email sent"}), 200
     except Exception as e:
         return jsonify({"Error": str(e)}), 400
+
+@auth_bp.route('/isloggedin', methods=["GET"])
+def is_logged_in():
+    if 'token' in session:
+        return jsonify({"logged_in": True, "email": session['email']}), 200
+    else:
+        return jsonify({"logged_in": False}), 200
