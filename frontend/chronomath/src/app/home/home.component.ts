4046785f851ff98aa6f62b0f8d71679f720b51df
Ahad0926 <ahadabdu0926@gmail.com>
2025-03-14 16:27:31 -0400

    Polished login and signup UI

--- frontend/chronomath/src/app/home/home.component.ts
+++ frontend/chronomath/src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { UserService } from '../services/user.service';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, AsyncPipe, CommonModule],
+  imports: [RouterLink, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
