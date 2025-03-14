c6c2a2e7f0395045e6a8a2e4e56e5fff56065878
Ahad0926 <ahadabdu0926@gmail.com>
2025-03-14 15:08:16 -0400

    Updated quiz page

---
+++ frontend/chronomath/src/app/quiz/quiz-sidebar/quiz-sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-sidebar',
+  imports: [],
+  templateUrl: './quiz-sidebar.component.html',
+  styleUrl: './quiz-sidebar.component.css'
+})
+export class QuizSidebarComponent {
+
+}
